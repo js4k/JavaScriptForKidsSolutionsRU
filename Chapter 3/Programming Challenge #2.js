c9f4936af3dd905f7612ae03a1f9c26903c21345
Phil Rigovanov <phil@rigovanov.ru>
2017-10-08 12:32:01 +0300

    challenge 3.2 translated

--- Chapter 3/Programming Challenge #2.js
+++ Chapter 3/Programming Challenge #2.js
@@ -1,13 +1,21 @@
-var randomBodyParts = ["Face", "Nose", "Hair"];
-var randomAnimalBodyParts = ["Tail", "Foot", "Fur"]
-var randomAdjectives = ["Smelly", "Boring", "Stupid"];
-var randomAnimals = ["Fly", "Marmot", "Monkey", "Rat"];
+var randomBodyParts = ["глаз", "нос", "череп"];
+var randomAnimalBodyParts = ["хвост", "пузо", "рот"];
+var randomAdjectives = ["вонючий", "унылый", "дурацкий"];
+var randomAnimals = ["жабы", "выдры", "коровы", "мартышки", "крысы"];
 
+// Выбор случайной части тела из массива randomBodyParts:
 var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
-var randomAnimalBodyPart = randomAnimalBodyParts[Math.floor(Math.random() * 3)];
-var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
-var randomAnimal = randomAnimals[Math.floor(Math.random() * 4)];
 
-var randomInsult = "Your " + randomBodyPart + " is more " + randomAdjective + " than a " + randomAnimal + "'s " + randomAnimalBodyPart;
+// Выбор случайной части тела животного из массива randomAnimalBodyParts:
+var randomAnimalBodyPart = randomAnimalBodyParts[Math.floor(Math.random() * 3)];
+
+// Выбор случайного прилагательного из массива randomAdjectives:
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+
+// Выбор случайного животного из массива randomAnimals:
+var randomAnimal = randomAnimals[Math.floor(Math.random() * 5)];
+
+// Соединяем случайные строки в предложение:
+var randomInsult = "У тебя " + randomBodyPart + " ещё более " + randomAdjective + " чем " + randomAnimalBodyPart + " у " + randomAnimal + "!!!";
 randomInsult;
-// "Your Nose is more Stupid than a Monkey's Tail"
+// "У тебя череп ещё более дурацкий чем хвост у мартышки!!!"
